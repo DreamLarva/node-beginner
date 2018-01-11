f13f100005dda0bf2ffffa977be9403e2c79355a
yangjiaqi <654136828@qq.com>
2018-01-11 11:12:28 +0800

    update

--- 无头浏览器/Puppeteer/2.js
+++ 无头浏览器/Puppeteer/2.js
@@ -4,8 +4,8 @@ const puppeteer = require('puppeteer');
 async function arrangeDate() {
 
     const section = [
-        [setTodayTime(9,30),setTodayTime(9,45)],
-        [setTodayTime(17,15),setTodayTime(17,30)]
+        [setTodayTime(9, 30), setTodayTime(9, 45)],
+        [setTodayTime(17, 15), setTodayTime(17, 30)]
     ];
 
     // 只要落在区间内 执行主进程
@@ -15,10 +15,9 @@ async function arrangeDate() {
     // todo
 
 
-
 }
 
-function setTodayTime(hours,minutes){
+function setTodayTime(hours, minutes) {
     const date = new Date();
     date.setHours(hours);
     date.setMinutes(minutes);
@@ -26,21 +25,61 @@ function setTodayTime(hours,minutes){
 
 }
 
-// arrangeDate();
+tempUse()
 
-async function main(section){
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto("http://itsp.orientsec.com.cn/dfzq-sign/sign.do");
-    await page.type('#userName','yangjiaqi',{delay:100});
-    await page.type('#password','a13579',{delay:100});
-    await page.click('#loginBtn');
-    await page.waitFor(2000);
+function tempUse() {
+    const nowTimeStamp = Date.now()
+    const target = new Date()
+    target.setHours(17);
+    target.setMinutes(15);
+    const targetTimeStamp = target.getTime()
+    setTimeout(() => main(), targetTimeStamp - nowTimeStamp)
+
 }
 
 
+async function main(section) {
+
+    if(valid()){
+        throw new Error("并不能不正确的时间执行")
+    }
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    // 进入页面
+    await page.goto("http://itsp.orientsec.com.cn/dfzq-sign/sign.do");
+    // 填写表单
+    await page.type('#userName', 'yangjiaqi', {delay: 100});
+    await page.type('#password', 'a13579', {delay: 100});
+    await page.click('#loginBtn');
+    // 等待跳转
+    await page.waitFor(2000);
+    // 执行a 或 b
+
+    const childFrames = page.mainFrame().childFrames();
+    const signFrame = childFrames[0];
+    const img = await signFrame.$$("img");
+    await img[1].click();
+
+
+    await page.waitFor(1000);
+
+    const signOut = await signFrame.$("#signOutDialog>div>*:last-child>a")
+
+    // await signOut.click();
+    //
+    // await page.waitFor(1000);
+    //
+    // await browser.close();
+
+
+}
+
+
+
+
+
 // 最后一次校验 保证不会在非正常时间执行
-function  valid() {
+function valid() {
     const date = new Date();
     return (
         date.getDay() === 6 ||
@@ -48,4 +87,8 @@ function  valid() {
         date.getHours() === 9 ||
         date.getHours() === 17
     )
+}
+
+function doSign() {
+
 }
