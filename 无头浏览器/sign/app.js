6471837e5f5953c811fe37bc0e707384fdc0de65
yangjiaqi <654136828@qq.com>
2018-02-07 12:23:03 +0800

    update

--- 无头浏览器/sign/app.js
+++ 无头浏览器/sign/app.js
@@ -1,34 +1,19 @@
 const puppeteer = require('puppeteer');
 const moment = require('moment');
 
-const section = [
-    // 周日
-    [],
-    // 周一
-    [["8:30", "8:40"], ["17:00", "17: 15"]],
-    // 周二
-    [["8:30", "8:40"], ["17:15", "17: 30"]],
-    // 周三
-    [["8:30", "8:40"], ["17:15", "17: 30"]],
-    // 周四
-    [["8:30", "8:40"], ["17:15", "17: 30"]],
-    // 周五
-    [["8:30", "8:40"], ["17:15", "17: 30"]],
-    // 周六
-    []
-];
+const section = require("./timeSpan");
+const user = require('./user');
+
 const STATUS = {
     SIGN_IN: Symbol(),
     SIGN_OUT: Symbol(),
 };
 
-const user = {
-    name: "yangjiaqi",
-    password: "a13579"
-};
+main(section)
+    .catch(console.log);
 
 
-async function main(section,date = moment()) {
+async function main(section, date = moment()) {
     const {offset, act} = isInSignTime(section, date);
 
     console.log('下次执行时间是' + moment(date.valueOf() + offset).format("YYYY年M月D日 dddd HH:mm"));
@@ -42,10 +27,6 @@ async function main(section,date = moment()) {
     }
 
 }
-
-main(section)
-    .catch(console.log);
-
 
 function isInSignTime(section, date) {
     // 获取 周几
@@ -157,28 +138,114 @@ async function sign(signSignal) {
     // 进入页面
     await page.goto("http://itsp.orientsec.com.cn/dfzq-sign/sign.do");
     // 填写表单
-    await page.type('#userName', user.name, {delay: 100});
-    await page.type('#password', user.password, {delay: 100});
-    await page.click('#loginBtn');
-    await page.waitFor(2000);
+    await page.type('#userName', user.name);
+    await page.type('#password', user.password);
+    // 点击跳转使用 waitForNavigation确保请求完成
+    await Promise.all([
+        page.click('#loginBtn'),
+        page.waitForNavigation({waitUntil: "networkidle2"})
+    ]);
 
-    const childFrames = page.mainFrame().childFrames();
-    const signFrame = childFrames[0];
-    const img = await signFrame.$$("img");
+    let signFrame = await selectSignTab(page);
+    let signButtons = await getSignButtons(signFrame);
 
-    await page.waitFor(2000);
     if (signSignal === STATUS.SIGN_OUT) {
-        await img[1].click();
+        // 签退情况
+        await signButtons[1].click();
+        await signFrame.waitForSelector("#signOutDialog > div > div:nth-child(1)");
+
+        if (await isWriteReport(signFrame)) {
+            // 还没有填写日报
+            console.log("开始填写日报");
+            // 切换到填写日报tab
+            const reportFrame = await selectReportTab(page);
+            // 填写今天的 周报
+            await inputReport(reportFrame);
+            // 保存
+            await (await reportFrame.$("body > div > div > h4 > a > img")).click();
+            // 切回签到页面
+            signFrame = await selectSignTab(page);
+            signButtons = await getSignButtons(signFrame);
+
+        }
+        // 开始签退
+        await signButtons[1].click();
+        await signFrame.waitForSelector("#signOutDialog > div > div:nth-child(1)");
+
+
     } else if (signSignal === STATUS.SIGN_IN) {
-        await img[0].click();
+        // 签到情况
+        // 开始签到
+        await signButtons[0].click();
+
     }
 
-    await page.waitFor(2000);
-    await (await signFrame.$("#signOutDialog > div > div:nth-child(3) > a:nth-child(1)")).click();
+    // 弹出框确定
 
+    Promise.all([
+        await (await signFrame.$("#signOutDialog > div > div:nth-child(3) > a:nth-child(1)")).click(),
+        page.waitForNavigation({waitUntil: "networkidle0"})
+    ]);
 
-    await page.waitFor(2000);
     await browser.close();
 
 }
 
+/**
+ * 切换到 签到的tab
+ * @return 签到页的frame
+ * */
+async function selectSignTab(page) {
+    const tab = await page.$("#easyui-tabs > div.tabs-header > div.tabs-wrap > ul > li:nth-child(1)");
+    await tab.click();
+
+    const childFrames = page.mainFrame().childFrames();
+    await childFrames[0].waitForSelector("body > table");
+    console.log("切换到签到tab");
+    return childFrames[0];
+}
+
+/**
+ * 切换到 周报的tab
+ * @return 周报的tab
+ * */
+async function selectReportTab(page) {
+    const tab = await page.$("#easyui-tabs > div.tabs-header > div.tabs-wrap > ul > li:nth-child(2)");
+    await tab.click();
+
+    const childFrames = page.mainFrame().childFrames();
+    await childFrames[1].waitForSelector("#kqlogForm");
+    console.log("切换到周报页");
+    return childFrames[1];
+}
+
+/**
+ * 获取 签到和 签退的按钮
+ * */
+async function getSignButtons(signFrame) {
+    return await signFrame.$$("body > div > a > img");
+}
+
+/**
+ * 判断是否填写了周报
+ * @return {boolean}
+ * */
+async function isWriteReport(page) {
+    return (await page.$eval("#signOutDialog > div > div:nth-child(1)", e => e.innerHTML)).includes("您的今日小结还未完成")
+}
+
+/**
+ * 填写今天的周报
+ * */
+async function inputReport(frame) {
+    const now = moment();
+    const date = now.format("YYYY-MM-DD");
+    const day =now.day();
+
+    const {byDate,byDay}= require('./report');
+    let inputContent = byDate[date] || byDay[day] || "";
+
+
+    const input = await frame.$(`#kqlogForm > table:nth-child(5) > tbody > tr:nth-child(${day}) > td > textarea`);
+    await input.type(inputContent)
+}
---
+++ 无头浏览器/sign/user.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Agent47 on 2018/2/7
+ * */
+"use strict";
+
+module.exports = {
+    name: "yangjiaqi",
+    password: "a13579"
+};
